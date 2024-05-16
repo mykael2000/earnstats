9fd6469d68133ae538676cf922c976515c48da34
Erameh Michael <eramehmichael2000@gmail.com>
2024-05-16 15:29:09 +0100

    upload

--- includes/footer.php
+++ includes/footer.php
@@ -15,10 +15,10 @@
         <p><strong>Email:</strong> <span>support@earnstats.com</span></p>
       </div>
       <div class="social-links d-flex mt-4">
-        <a href=""><i class="bi bi-twitter"></i></a>
+        <!-- <a href=""><i class="bi bi-twitter"></i></a>
         <a href=""><i class="bi bi-facebook"></i></a>
-        <a href=""><i class="bi bi-instagram"></i></a>
-        <a href=""><i class="bi bi-linkedin"></i></a>
+        <a href=""><i class="bi bi-instagram"></i></a> -->
+        <a href="https://www.youtube.com/@Earnstats9899"><i class="bi bi-youtube"></i></a>
       </div>
     </div>
 
